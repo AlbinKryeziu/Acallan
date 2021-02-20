d03558b2060f0c60c73c8acb616b9b7119232bc8
AlbinKryeziu <57589969+AlbinKryeziu@users.noreply.github.com>
2021-02-20 11:05:32 +0100

    fixes

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -3,6 +3,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
@@ -73,227 +74,212 @@
             </div>
         </div>
     </div>
-    @php $date = Carbon\Carbon::today()->toDateTimeString(); $event = App\Models\Event::with(['requestEvent' => function($q){ $q->where('status', 1); }])->where('user_id',Auth::id())->whereDate('start',$date)->where('status',1)->get();
-    @endphp
+    @php $date = Carbon\Carbon::today()->toDateTimeString(); $events = App\Models\Event::with(['requestEvent' => function($q){ $q->where('status', 1);
+    }])->where('user_id',Auth::id())->whereDate('start',$date)->where('status',1)->paginate(5); @endphp
 
-    <div class="p-4">
-        <div class="bg-white p-1">
-            <div class="table">
-                <div class="">
-                    <div class="table-title">
-                        <div class="row"></div>
-                    </div>
-
-                    <table class="table table-hover table-bordered"></table>
-                    <div class="table-title">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <br />
-                            </div>
-                        </div>
-                    </div>
-
-                    <table class="table table-hover table-bordered">
-                        <tr></tr>
-                        <tr>
-                            <th colspan="8" style="text-align: center;">Today Events</th>
-                        </tr>
-                        @if($event->count() >= 1)
-                            
-                       
-                        <th colspan="2">#</th>
-                        <th colspan="">Event Name</th>
-                        <th colspan="">Start Date</th>
-                        <th colspan="">End Date</th>
-                        <th colspan="">Name of Client</th>
-                        <tbody>
-                            @foreach($event as $key => $event)
-                            <tr>
-                                <td colspan="2">#</td>
-                                <td colspan="">{{ $event->title }}</td>
-                                <td colspan="">{{ $event->start}}</td>
-                                <td colspan="">{{ $event->end}}</td>
-                                <td colspan="">{{ $event->requestEvent->first()->requestClient->name}}</td>
-                            </tr>
-                            @endforeach
-                            @else
-                          
-                        </tbody>
-                    </table>
-                    <div class="alert alert-secondary" role="alert">
-                        No meeting for today so far
-                      </div> 
+    <div class="max-w-7xl p-4">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th colspan="6" scope="col" style="text-align: center;">Event For Today</th>
+                </tr>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Event Name</th>
+                    <th scope="col">Start Date</th>
+                    <th scope="col">End Date</th>
+                    <th scope="col">Client Name</th>
+                    <th scope="col">Zoom</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($events as $key => $event)
+                 @php  $date = Carbon\Carbon::parse($event->start)->format('Y-m-d H:i'); 
+                 $carbon = Carbon\Carbon::now(); 
+                 echo $diference =$carbon->diffInHours($date);
+                  @endphp
+                <tr>
+                    <th scope="row">1</th>
+                    <td>{{ $event->title }}</td>
+                    <td colspan="">{{ $event->start}}</td>
+                    <td colspan="">{{ $event->end}}</td>
+                    <td colspan="">{{ $event->requestEvent->first()->requestClient->name}}</td>
+                    @if($diference <= '1')
+                    <td colspan=""><i class="fa fa-eercast" aria-hidden="true"></i></td>
                     @endif
+                </tr>
+
+                @endforeach
+            </tbody>
+            {{ $events->links() }}
+        </table>
+    </div>
+
+    <div class="container py-4">
+        <div class="response alert alert-success mt-2" style="display: none;"></div>
+        <div id="calendar"></div>
+    </div>
+    @endif
+    <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    ...
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-danger btn-ok">Delete</a>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="container py-4">
-    <div class="response alert alert-success mt-2" style="display: none;"></div>
-    <div id="calendar"></div>
-</div>
-@endif
-<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                ...
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-danger btn-ok">Delete</a>
-            </div>
-        </div>
-    </div>
-</div>
-
-@if (auth()->user()->hasRole('client'))
-<div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 bg-gray-200">
-    <div class="overflow-hidden sm:rounded-md">
-        <div class="px-4 py-5 bg-white sm:p-6">
-            <table>
-                <th style="text-align: left;">Available Doctors</th>
-                <th style="text-align: left;"></th>
-                {{-- @if(is_null(App\Models\User::getUsersByRole('doc')))
-                <p>Not</p>
-                @else @foreach (App\Models\User::getUsersByRole('doc') as $doctor)
-                <tr>
-                    <td style="width: 50%;">{{$doctor->name}}</td>
-                    <td>
-                        <div class="button px-4" style="background-color: #ed1b24;"><a href="{{url('/')}}" style="color: white;">REQUEST A MEETING</a></div>
-                    </td>
-                </tr>
-                @endforeach @endif --}}
-            </table>
-        </div>
-    </div>
-</div>
-
-<div class="modal" tabindex="-1" role="dialog" id="bini">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Modal body text goes here.</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Save changes</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    @if (auth()->user()->hasRole('client'))
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 bg-gray-200">
+        <div class="overflow-hidden sm:rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+                <table>
+                    <th style="text-align: left;">Available Doctors</th>
+                    <th style="text-align: left;"></th>
+                    {{-- @if(is_null(App\Models\User::getUsersByRole('doc')))
+                    <p>Not</p>
+                    @else @foreach (App\Models\User::getUsersByRole('doc') as $doctor)
+                    <tr>
+                        <td style="width: 50%;">{{$doctor->name}}</td>
+                        <td>
+                            <div class="button px-4" style="background-color: #ed1b24;"><a href="{{url('/')}}" style="color: white;">REQUEST A MEETING</a></div>
+                        </td>
+                    </tr>
+                    @endforeach @endif --}}
+                </table>
             </div>
         </div>
     </div>
-</div>
-@endif
 
-<script>
-    $(document).ready(function () {
-        var SITEURL = "{{ url('/') }}";
+    <div class="modal" tabindex="-1" role="dialog" id="bini">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 
-        $.ajaxSetup({
-            headers: {
-                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            },
-        });
+    <script>
+        $(document).ready(function () {
+            var SITEURL = "{{ url('/') }}";
 
-        var calendar = $("#calendar").fullCalendar({
-            editable: true,
-            events: SITEURL + "/fullcalender",
-            displayEventTime: false,
-            editable: true,
+            $.ajaxSetup({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                },
+            });
 
-            eventRender: function (event, element, view) {
-                if (event.allDay === "true") {
-                    event.allDay = true;
-                } else {
-                    event.allDay = false;
-                }
-            },
-            selectable: true,
-            selectHelper: true,
-            select: function (start, end, allDay) {
-                var title = prompt("Event Title:");
-                if (title) {
-                    var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
-                    var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
+            var calendar = $("#calendar").fullCalendar({
+                editable: true,
+                events: SITEURL + "/fullcalender",
+                displayEventTime: false,
+                editable: true,
+
+                eventRender: function (event, element, view) {
+                    if (event.allDay === "true") {
+                        event.allDay = true;
+                    } else {
+                        event.allDay = false;
+                    }
+                },
+                selectable: true,
+                selectHelper: true,
+                select: function (start, end, allDay) {
+                    var title = prompt("Event Title:");
+                    if (title) {
+                        var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
+                        var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
+                        $.ajax({
+                            url: SITEURL + "/fullcalendareventmaster/create",
+                            data: {
+                                title: title,
+                                start: start,
+                                end: end,
+                                type: "add",
+                            },
+                            type: "POST",
+                            success: function (data) {
+                                displayMessage("Event Created Successfully");
+
+                                calendar.fullCalendar(
+                                    "renderEvent",
+                                    {
+                                        id: data.id,
+                                        title: title,
+                                        start: start,
+                                        end: end,
+                                        allDay: allDay,
+                                    },
+                                    true
+                                );
+
+                                calendar.fullCalendar("unselect");
+                            },
+                        });
+                    }
+                },
+                eventDrop: function (event, delta) {
+                    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
+                    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
+
                     $.ajax({
-                        url: SITEURL + "/fullcalendareventmaster/create",
+                        url: SITEURL + "/fullcalendareventmaster/update",
                         data: {
-                            title: title,
+                            title: event.title,
                             start: start,
                             end: end,
-                            type: "add",
-                        },
-                        type: "POST",
-                        success: function (data) {
-                            displayMessage("Event Created Successfully");
-
-                            calendar.fullCalendar(
-                                "renderEvent",
-                                {
-                                    id: data.id,
-                                    title: title,
-                                    start: start,
-                                    end: end,
-                                    allDay: allDay,
-                                },
-                                true
-                            );
-
-                            calendar.fullCalendar("unselect");
-                        },
-                    });
-                }
-            },
-            eventDrop: function (event, delta) {
-                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
-                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
-
-                $.ajax({
-                    url: SITEURL + "/fullcalendareventmaster/update",
-                    data: {
-                        title: event.title,
-                        start: start,
-                        end: end,
-                        id: event.id,
-                        type: "update",
-                    },
-                    type: "POST",
-                    success: function (response) {
-                        displayMessage("Event Updated Successfully");
-                    },
-                });
-            },
-            eventClick: function (event) {
-                deleteMsg = confirm("Are you sure for this");
-                if (deleteMsg) {
-                    $.ajax({
-                        type: "POST",
-                        url: SITEURL + "/fullcalendareventmaster/delete",
-                        data: {
                             id: event.id,
-                            type: "delete",
+                            type: "update",
                         },
+                        type: "POST",
                         success: function (response) {
-                            calendar.fullCalendar("removeEvents", event.id);
-                            displayMessage("Event Deleted Successfully");
+                            displayMessage("Event Updated Successfully");
                         },
                     });
-                }
-            },
+                },
+                eventClick: function (event) {
+                    deleteMsg = confirm("Are you sure for this");
+                    if (deleteMsg) {
+                        $.ajax({
+                            type: "POST",
+                            url: SITEURL + "/fullcalendareventmaster/delete",
+                            data: {
+                                id: event.id,
+                                type: "delete",
+                            },
+                            success: function (response) {
+                                calendar.fullCalendar("removeEvents", event.id);
+                                displayMessage("Event Deleted Successfully");
+                            },
+                        });
+                    }
+                },
+            });
         });
-    });
 
-    function displayMessage(message) {
-        toastr.success(message, "Event");
-    }
-</script>
+        function displayMessage(message) {
+            toastr.success(message, "Event");
+        }
+    </script>
+</div>
