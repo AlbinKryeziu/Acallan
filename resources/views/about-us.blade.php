9a5e1fc6a0762be0a320b679cd893ffc166b6351
Art <art.kujtimi@hotmail.com>
2021-05-03 12:01:17 +0200

    Language

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -20,7 +20,7 @@
 
     <div class="page-banner" style="background-image: url('../images/about-us/banner.jpg');">
         <div class="container">
-            <h1 class="banner-title center"><span class="red-text">About</span> Us </h1>
+            <h1 class="banner-title center"><span class="red-text">@lang('about_page_title_about')</span> @lang('about_page_title_us') </h1>
         </div>
     </div>
 
@@ -46,12 +46,12 @@
 
     <div class="section section-2">
         <div class="container content">
-            <h1 class="title">HOW</h1>
-            <p class="center">We built a digital platform to connect pharma companies to Doctors</p>
+            <h1 class="title">@lang('how')</h1>
+            <p class="center">@lang('shadow_box_title')</p>
 
             <div class="shadow p-3 mb-5 bg-white rounded flex">
                 <img src="images/Online Doctor-rafiki.jpg" alt="">
-                <h4 class="my-auto">The platform supports the goal of continues care for doctors and manages value for their time and access to pharma industry by providing virtual interface with pharma companies</h4>
+                <h4 class="my-auto">@lang('shadow_box_4')</h4>
             </div>
 
             <div class="shadow p-3 mb-5 bg-white rounded flex">
@@ -91,7 +91,7 @@
                 <h4 class="my-auto">Increase number of pharma company visits by getting doctor’s undivided attention to establish efficient, convenient and responsive ways to communicate with doctors. </h4>
             </div>
 
-            <p class="center">Technology helps build cutting edge solutions like <strong>ProMedRep</strong></p>
+            <p class="center">@lang('home_banner')</p>
             <p class="center">This is why the best way to meet the needs of pharma companies and doctors is enable them to meet securely and in a timely fashion </p>
 
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,29 +42,29 @@
             <div class="box-container flex">
                 <div class="shadow-box center shadow-box-1">
                     <img src="images/Meeting-bro.jpg" alt="">
-                    <h2>Enable healthcare providers to stay connected with pharma industry and manage their interviews virtually </h2>
+                    <h2>@lang('shadow_box_1')</h2>
                 </div>
                 <div class="shadow-box center shadow-box-2">
                     <img src="images/Social strategy-amico.jpg" alt="">
-                    <h2>Help pharma industry to optimize their efforts and enhance quality promotion of their products remotely with their clients </h2>
+                    <h2>@lang('shadow_box_2')</h2>
                 </div>
                 <div class="shadow-box center shadow-box-3">
                     <img src="images/Social strategy-pana.jpg" alt="">
-                    <h2>Create value for health care provider and the pharma industry </h2>
+                    <h2>@lang('shadow_box_3')</h2>
                 </div>
             </div>
         </div>
 
         <div class="container content" style="padding-top: 50px;">
-            <p class="center">We built a digital platform to connect pharma companies to Doctors</p>
+            <p class="center">@lang('shadow_box_title')</p>
             <div class="box-container flex">
                 <div class="shadow-box center half-width shadow-box-4">
                     <img class="m-auto" src="images/Online Doctor-rafiki.jpg" alt="">
-                    <h2>The platform supports the goal of continues care for doctors and manages value for their time and access to pharma industry by providing virtual interface with pharma companies</h2>
+                    <h2>@lang('shadow_box_4')</h2>
                 </div>
                 <div class="shadow-box center half-width shadow-box-5">
                     <img class="m-auto" src="images/Insurance-pana.jpg" alt="">
-                    <h2>The virtual platform also supports pharma industries, marketing and sales management functions and online management for better client engagement</h2>
+                    <h2>@lang('shadow_box_5')</h2>
                 </div>
             </div>
         </div>
@@ -72,9 +72,9 @@
 
     <div class="banner-scroll bg-overlay" style="background-image: url(images/online-marketing-hIgeoQjS_iE-unsplash.jpg);">
         <div class="container content">
-            <p class="banner-sub-title center" style="font-size: 32px;">Technology helps build cutting edge solutions like ProMedRep</p>
+            <p class="banner-sub-title center" style="font-size: 32px;">@lang('home_banner')</p>
             <div class="buttons flex">
-                <div class="button" style="background-color: #ED1B24;"><a href="{{url('/register')}}" style="color: white;">CREATE A NEW ACCOUNT</a></div>
+                <div class="button" style="background-color: #ED1B24;"><a href="{{url('/register')}}" style="color: white;">@lang('create_new_account')</a></div>
             </div>
         </div>
     </div>
