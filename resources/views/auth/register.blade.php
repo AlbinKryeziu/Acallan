c3d6b9697dd839df113f3f96530caf2c66822326
AlbinKryeziu <57589969+AlbinKryeziu@users.noreply.github.com>
2021-05-05 11:58:43 +0200

    fixes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,47 +10,47 @@
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <x-jet-label for="name" value="{{ trans('name') }}" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-label for="email" value="{{ trans('email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
             <div class="mt-4">
-                <x-jet-label for="address" value="{{ __('Address') }}" />
+                <x-jet-label for="address" value="{{ trans('address') }}" />
                 <x-jet-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autofocus autocomplete="name" />
             </div>
             <div class="mt-4">
-                <x-jet-label for="state" value="{{ __('State') }}" />
+                <x-jet-label for="state" value="{{ trans('state') }}" />
                 <x-jet-input id="state" class="block mt-1 w-full" type="text" name="state" :value="old('state')" required autofocus autocomplete="name" />
             </div>
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ trans('password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-label for="password_confirmation" value="{{ trans('confirm_password') }}" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
             <div class="mt-4">
-                <x-jet-label for="role" value="{{ __('Select position') }}" />
+                <x-jet-label for="role" value="{{ trans('select_postion') }}" />
                <select name="role" class="block mt-1 w-full form-input rounded-md shadow-sm" required autofocus autocomplete="role">
-                <option value="">Choose an option</option>
-                   <option value="4">Manager</option>
-                   <option value="3">Client</option>
+                <option value="">@lang('chose_option')</option>
+                   <option value="4">@lang('manger')</option>
+                   <option value="3">@lang('client')</option>
                </select>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                   @lang('alredy_register')
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    @lang('register')
                 </x-jet-button>
             </div>
         </form>
