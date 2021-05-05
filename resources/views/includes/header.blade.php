3f7bc0c478fc5cce3ae8f4aa031d1d87b49bca3a
AlbinKryeziu <57589969+AlbinKryeziu@users.noreply.github.com>
2021-05-05 16:09:23 +0200

    responsive nav

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,29 +1,51 @@
-<header>
-    <div class="container flex">
-        <div class="logo">
-            <img src="images/logo.jpg" alt="">
-        </div>
-        <div class="menu">
-            <ul>
-                <li><a href="{{url('/')}}">{{__('home')}}</a></li>
-                <li><a href="{{url('/about-us')}}">@lang('aboutUs')</a></li>
-                <li><a href="{{url('/how-it-works')}}">@lang('howItWork')</a></li>
-                <li><a href="{{url('/contact-us')}}">@lang('contactUs')</a></li>
-                @guest
-                <li>
-                    <div class="dropdown">
-                        <button class="dropbtn">@lang('login')</button>
-                        <div class="dropdown-content">
-                            <a href="{{url('/login')}}">@lang('login')-@lang('doctor')</a>
-                            <a href="{{url('/login')}}">@lang('login')-@lang('client')</a>
-                        </div>
-                    </div>
-                </li>
-                @endguest
-                <li><a href="lang/en">En</a></li>
-                <li><a href="lang/es">Es</a></li>
-            </ul>
-        </div>
+<nav class="navbar navbar-expand-lg bg-white fixed-top">
+  <style>
+    .carousel-item {
+  height: 65vh;
+  min-height: 350px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+  </style>
+  <div class="container">
+    <img src="images/logo.jpg" alt="" style="width: 180px; left:850px">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{ url('/') }}">@lang('home')
+                <span class="sr-only"></span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/about-us') }}">@lang('aboutUs')</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/how-it-works') }}">@lang('howItWork')</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/contact-us') }}">@lang('contactUs')</a>
+        </li>
+        <li class="nav-item">
+          <div class="dropdown nav-item">
+            <button class="dropbtn">@lang('login')</button>
+            <div class="dropdown-content">
+                <a href="{{url('/login')}}">@lang('login')-@lang('doctor')</a>
+                <a href="{{url('/login')}}">@lang('login')-@lang('client')</a>
+            </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="lang/en">En</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="lang/es">Es</a>
+        </li>
+          </ul>
     </div>
-</header>
-
+  </div>
+</nav>
